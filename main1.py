9b4ed5464ff412299c51895d35ddb3a77fd45f32
AutocraticKhan <Autocratic.jalal@gmail.com>
2023-12-02 23:52:54 +0500

    dv

--- main1.py
+++ main1.py
@@ -86,7 +86,7 @@ with open('config.yml', 'r') as file:
 json_file_path = 'history.json'
 
 chrome_options = Options()
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-gpu')
